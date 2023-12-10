0bd4e58799b3e40b6ff479a50f3e430c95116d87
Thevolumetrader <147925265+Thevolumetrader@users.noreply.github.com>
2023-12-10 09:15:54 +0000

    updated the codes for seeding and migrating relationship database

---
+++ database/factories/Backend/TagFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories\Backend;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Backend\Tag>
+ */
+class TagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->text(7);
+        return [
+            'name' =>  $name,
+            'slug' =>Str::slug($name),
+        ];
+    }
+}
